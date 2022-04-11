ced50baff0d82eed7f3dce63eb4a8cb8f5473992
MohsinMalik2 <malikmuhsin70@gmail.com>
2022-04-11 15:53:14 +0500

    Depositing tokens to d bank for stake functions

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -16,4 +16,20 @@ contract DecentralBank {
         tether = _tether;
     } 
 
+   
+//Staking FUnction
+    function depositTokens(uint256 _amount) public {
+
+
+         //Transfering tether tokens to thius contract to stake
+
+        tether.transferFrom(msg.sender,address(this), _amount);
+
+        // Transfer means transfering directly or transfer happened by the owner of the token
+        //TransferfROM REFERS to the transfering of token through third party after the approval of the owner.
+
+
+        
+    }
+
 }
--- test/DecentralBank.tests.js
+++ test/DecentralBank.tests.js
@@ -7,16 +7,31 @@ require("chai")
 .use(require('chai-as-promised'))
 .should()
 
-contract('DecentralBank',(accounts) => {
+
+//accounts array can be factorized by giving every index a name to make the code easy to understand and readable
+// contract('DecentralBank',(accounts) => {
+contract('DecentralBank',([owner, customer]) => {
     //All the code will go here for testing
 
-    let tether, rwd
-
+    let tether, rwd, dBank
+    function converter(number){
+        return web3.utils.toWei(number , 'ether')
+    }
     //In test Before function apply before everything else
     
     before(async () => {
         tether = await Tether.new();
         rwd = await RWD.new();
+        dBank = await DecentralBank.new(rwd.address, tether.address);
+
+        // Transfer all tokens to decentral bank
+
+        await rwd.transferTo(dBank.address , converter('1000000'))
+
+        //transfer 100 mock tokens to customer accounts
+
+        // await tether.transferTo(accounts[1],converter('100'),{from: accounts[0]}); old way when use array name
+        await tether.transferTo(customer,converter('100'),{from: owner});  // new way when use index names
     })  
 
     // 1st test to check the name of the tether token (assertion)
@@ -42,5 +57,24 @@ contract('DecentralBank',(accounts) => {
         })
     })
 
+    // 3rd test to check the amount of the Reward tokens inside decentral bank (assertion)
+
+
+    describe("Reward Token Amount Test", async () => {
+        
+        
+        it("DBank Name Test", async () => {
+            const name =  await dBank.name();
+
+            assert.equal(name , 'Decentral Bank')    
+        })
+
+
+        it("DBank Has Tokens", async () => {
+            let balance = await rwd.balanceOf(dBank.address);
+
+            assert.equal(balance , converter('1000000'))   
+        })
+    })
 
 })
